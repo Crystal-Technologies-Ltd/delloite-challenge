187adbef7c359b01eb7352f98b2ed814ab0acddd
Nguyen Truong Giang <giang.nguyen@crystal-techs.com>
2020-11-24 00:02:24 +0700

    progress - Movie detail UI, movie detail fragment

--- app/src/main/java/com/giang/delloitechallenge/moviedetail/MovieDetailFragment.kt
+++ app/src/main/java/com/giang/delloitechallenge/moviedetail/MovieDetailFragment.kt
@@ -1,2 +1,13 @@
 package com.giang.delloitechallenge.moviedetail
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.giang.delloitechallenge.R
+
+class MovieDetailFragment : Fragment(R.layout.fragment_movies_detail) {
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+  }
+}
